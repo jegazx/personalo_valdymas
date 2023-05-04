c870a0cc3eea20faaaa0074747d3ac0a26432782
Mindaugas Turauskas <min.turauskas@gmail.com>
2023-05-04 16:08:20 +0300

    Galutinis variantas

--- gui.py
+++ gui.py
@@ -1,28 +1,28 @@
 import PySimpleGUI as sg
 from datetime import date
 from personalo_valdymas import PersonaloValdymas
-
+import json
 
 def sarasas():
     for darbuotojas in darbuotojai._PersonaloValdymas__darbuotojai.values():
-        td.append([darbuotojas['vardas_pavarde'], darbuotojas['komentaras'], darbuotojas['alga'], darbuotojas['priimtas'], darbuotojas['atleistas'], darbuotojas['tel_numeris'], darbuotojas['ak'], darbuotojas['issilavinimas'], darbuotojas['padalinys']])
+        td.append([darbuotojas['vardas_pavarde'], darbuotojas['komentaras'], darbuotojas['alga'], darbuotojas['priimtas'], darbuotojas['tel_numeris'], darbuotojas['ak'], darbuotojas['issilavinimas'], darbuotojas['padalinys']])
 
 darbuotojai = PersonaloValdymas()
 darbuotojai = PersonaloValdymas.pickle_nuskaitymas(darbuotojai)
 td=[]
-Headings=['Vardas Pavardė', 'Komentaras', 'Alga', 'Priimtas', 'Atleistas', 'Telefono numeris', 'Asmens kodas', 'Išsilavinimas', 'Skyrius']
+headings=['Vardas Pavardė', 'Komentaras', 'Alga', 'Priimtas', 'Telefono numeris', 'Asmens kodas', 'Išsilavinimas', 'Skyrius']
+
 layout=[
-        [sg.Text(Headings[0], size=(15,1)),sg.Input(size=20,key=Headings[0])],
-        [sg.Text(Headings[1], size=(15,1)),sg.Input(size=20,key=Headings[1])],
-        [sg.Text(Headings[2], size=(15,1)),sg.Input(size=20,key=Headings[2])],
-        [sg.Text(Headings[3], size=(15,1)),sg.Input(size=20,key=Headings[3])],
-        [sg.Text(Headings[4], size=(15,1)),sg.Input(size=20,key=Headings[4])],
-        [sg.Text(Headings[5], size=(15,1)),sg.Input(size=20,key=Headings[5])],
-        [sg.Text(Headings[6], size=(15,1)),sg.Input(size=20,key=Headings[6])],
-        [sg.Text(Headings[7], size=(15,1)),sg.Input(size=20,key=Headings[7])],
-        [sg.Text(Headings[8], size=(18,1)),sg.Combo(['IT skyrius','Finansai','Administracija'],key=Headings[8])],
+        [sg.Text(headings[0], size=(15,1)),sg.Input(size=20,key=headings[0])],
+        [sg.Text(headings[1], size=(15,1)),sg.Input(size=20,key=headings[1])],
+        [sg.Text(headings[2], size=(15,1)),sg.Input(size=20,key=headings[2])],
+        [sg.Text(headings[3], size=(15,1)),sg.Input(size=20,key=headings[3])],
+        [sg.Text(headings[4], size=(15,1)),sg.Input(size=20,key=headings[4])],
+        [sg.Text(headings[5], size=(15,1)),sg.Input(size=20,key=headings[5])],
+        [sg.Text(headings[6], size=(15,1)),sg.Input(size=20,key=headings[6])],
+        [sg.Text(headings[7], size=(18,1)),sg.Combo(['IT skyrius','Finansai','Administracija'],key=headings[7])],
         [sg.Button('Pridėti'), sg.Button('Redaguoti'), sg.Button('Išsaugoti',disabled=True), sg.Button('Ištrinti'), sg.Push(), sg.Exit()],
-        [sg.Table(td,Headings,key='myTable')]]
+        [sg.Table(td,headings,key='myTable')]]
 
 window=sg.Window('Personalo valdymo programa',layout)
 sarasas()
@@ -30,26 +30,26 @@ while True:
     event,values= window.read()
     print (values)
     if event == 'Pridėti':
-        td.append([values[Headings[0]], values[Headings[1]], values[Headings[2]], values[Headings[3]], values[Headings[4]], values[Headings[5]], values[Headings[6]], values[Headings[7]], values[Headings[8]]])
-        darbuotojai.prideti_darbuotoja(values[Headings[0]], values[Headings[1]], values[Headings[2]], values[Headings[3]], values[Headings[4]], values[Headings[5]], values[Headings[6]], values[Headings[7]], values[Headings[8]])
+        values = [values[h] for h in headings]
+        td.append(values)
+        darbuotojai.prideti_darbuotoja(*values)
         darbuotojai.darbuotoju_sarasas()
         window['myTable'].update(values=td)
-        for i in range(9):    
-            window[Headings[i]].update(value='')
+        for i in range(len(headings)):    
+            window[headings[i]].update(value='')
     if event == 'Redaguoti':
         if values['myTable']==[]:
-            sg.popup('Pasirinkite eilutę')
+            sg.popup('Pasirinkite eilutę, kurią norite redaguoti')
         else:
             editRow=values['myTable'][0]
-            sg.popup('Redaguoti')
-            for i in range(9):  
-                window[Headings[i]].update(value=td[editRow][i])
+            for i in range(len(headings)):  
+                window[headings[i]].update(value=td[editRow][i])
             window['Išsaugoti'].update(disabled=False)
     if event == 'Išsaugoti':
-        td[editRow]=([values[Headings[0]],values[Headings[1]], values[Headings[2]], values[Headings[3]], values[Headings[4]], values[Headings[5]], values[Headings[6]], values[Headings[7]], values[Headings[8]]])
+        td[editRow]=([values[h] for h in headings])
         window['myTable'].update(values=td)
-        for i in range(9):
-            window[Headings[i]].update(value='')
+        for i in range(len(headings)):
+            window[headings[i]].update(value='')
         window['Išsaugoti'].update(disabled=True)
     if event == 'Ištrinti':
         if values['myTable']==[]:
@@ -66,7 +66,6 @@ while True:
                 darbuotojai.atleistu_darbuotoju_sarasas()
                 darbuotojai.darbuotoju_sarasas()
                 window['myTable'].update(values=td)
-
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
 window.close()
--- personalo_valdymas.py
+++ personalo_valdymas.py
@@ -24,13 +24,6 @@ def create_logger(logger_name, log_file):
 
 logger = create_logger('logging', 'personalo_valdymas.log')
 logger.info('Paleista programa')
-
-def clear():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-
 padaliniai = ["IT skyrius", "Finansai", "Administracija"]
 
 class DateEncoder(json.JSONEncoder):
@@ -56,8 +49,8 @@ class PersonaloValdymas():
         self.__darbuotojai = {}
         self.__atleisti_darbuotojai = {}
         
-    def prideti_darbuotoja(self, vardas_pavarde, komentaras, alga, priimtas, atleistas, tel_numeris, ak, issilavinimas, padalinys):
-        darbuotojas = {'vardas_pavarde': vardas_pavarde, 'komentaras': komentaras, 'alga': alga, 'priimtas': priimtas, 'atleistas': atleistas, 'tel_numeris': tel_numeris, 'ak': ak, 'issilavinimas': issilavinimas, 'padalinys': padalinys}
+    def prideti_darbuotoja(self, vardas_pavarde, komentaras, alga, priimtas, tel_numeris, ak, issilavinimas, padalinys, atleistas=""):
+        darbuotojas = {'Vardas Pavardė': vardas_pavarde, 'Komentaras': komentaras, 'Alga': alga, 'Priimtas': priimtas, 'Telefono numeris': tel_numeris, 'Asmens kodas': ak, 'Išsilavinimas': issilavinimas, 'Padalinys': padalinys, 'Atleistas': atleistas}
         self.__darbuotojai[vardas_pavarde] = darbuotojas
         print(f"{vardas_pavarde} buvo pridėtas")
         logger.info(f'Pridėtas darbuotojas: "{vardas_pavarde}"')
@@ -66,7 +59,7 @@ class PersonaloValdymas():
     def atleisti_darbuotoja(self, vardas_pavarde, atleistas):
         darbuotojas = self.__darbuotojai.pop(vardas_pavarde, None)
         if darbuotojas:
-            darbuotojas['atleistas'] = atleistas
+            darbuotojas['Atleistas'] = atleistas
             self.__atleisti_darbuotojai[vardas_pavarde] = darbuotojas
             logger.info(f'Atleistas darbuotojas: "{vardas_pavarde}"')
             PersonaloValdymas.pickle_sukurimas(self)
@@ -76,7 +69,7 @@ class PersonaloValdymas():
         if darbuotojas:
             vardas, pavarde = vardas_pavarde.split()
             vardas = naujas_vardas
-            darbuotojas['vardas_pavarde'] = vardas + " " + pavarde
+            darbuotojas['Vardas Pavardė'] = vardas + " " + pavarde
             logger.info(f'Pakeistas darbuotojas vardas. Naujas vardas ir pavardė: "{vardas} {pavarde}"')
             self.__darbuotojai[darbuotojas['vardas_pavarde']] = darbuotojas
             PersonaloValdymas.pickle_sukurimas(self)
@@ -134,7 +127,7 @@ class PersonaloValdymas():
             return darbuotojas['vardas_pavarde'] 
 
     def darbuotoju_sarasas(self):
-        print("Darbuotoju sarasas")
+        print("Darbuotojų sąrašas")
         print(list(self.__darbuotojai.values()))
         with open('Darbuotoju_sarasas.json', 'w') as f:
             json.dump(self.__darbuotojai, f, indent=4)
@@ -151,7 +144,7 @@ class PersonaloValdymas():
         return self.__atleisti_darbuotojai
     
     def atleistu_darbuotoju_sarasas(self):
-        print("Atleistu darbuotoju sarasas")
+        print("Atleistų darbuotojų sąrašas")
         print(list(self.__atleisti_darbuotojai.values()))
         with open('Atleistu_darbuotoju_sarasas.json', 'w') as f:
             json.dump(self.__atleisti_darbuotojai, f, indent=4, cls=DateEncoder)
@@ -162,7 +155,7 @@ class PersonaloValdymas():
         with open("Darbuotoju_duomenys.pickle", "wb") as f:
             pickle.dump(self.__darbuotojai, f)
             pickle.dump(self.__atleisti_darbuotojai, f)
-            logger.info('Pickle sukūrinmas')
+            logger.info('Pickle sukūrimas')
         return self
     
     def pickle_nuskaitymas(self):
@@ -171,27 +164,3 @@ class PersonaloValdymas():
             self.__atleisti_darbuotojai = pickle.load(f)
             logger.info('Pickle nuskaitymas')
         return self
-
-# if __name__ == "__main__":
-darbuotojai = PersonaloValdymas()
-darbuotojai = PersonaloValdymas.pickle_nuskaitymas(darbuotojai)
-# darbuotojai.prideti_darbuotoja("Algis Algimantas", "sokejas", 1111, "2002-05-12", None, "1234567988", "987654321", "traktoristas", "it")
-# darbuotojai.prideti_darbuotoja("Algimante Algimantas", "sokejas", 1111, "2002-05-12", None, "1234567988", "987654321", "traktoristas", "it")
-# darbuotojai.darbuotoju_sarasas()
-# darbuotojai.atleisti_darbuotoja("Algis Algimantas", "2023-05-02")
-# darbuotojai.atleisti_darbuotoja("Algimante Algimantas", "2023-05-02")
-# darbuotojai.darbuotoju_sarasas()
-# darbuotojai.atleistu_darbuotoju_sarasas()
-# darbuotojai.keisti_alga("Algimante Algimantas", 2555)
-# darbuotojai.keisti_alga("Algimante Algimantas", 1478)
-# darbuotojai.darbuotoju_sarasas()
-
-# darbuotojai.keisti_vardas("Algis Algimantas", "Pienius")
-# darbuotojai.darbuotoju_sarasas()
-
-# logger.info('Programa išjungta')
-# darbuotojai.keisti_vardas("Algis Algimantas", "Pienius")
-# darbuotojai.darbuotoju_sarasas()
-
-print(darbuotojai._PersonaloValdymas__darbuotojai)
-
